a59879e42debe182d12236768450e845c718d09c
nfor2000 <takoppresly@gmail.com>
2024-07-30 06:36:32 +0100

    mobile design completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,41 @@
-const navMenu = document.getElementById('nav-menu')
-const openMenuBtn = document.getElementById('open-menu')
-const closeMenuBtn = document.getElementById('close-menu')
-const overlay = document.getElementById('overlay')
-const navLinks = Array.from(document.querySelectorAll('.nav-link'))
+const navMenu = document.getElementById("nav-menu");
+const openMenuBtn = document.getElementById("open-menu");
+const closeMenuBtn = document.getElementById("close-menu");
+const overlay = document.getElementById("overlay");
+const navLinks = Array.from(document.querySelectorAll(".nav-link"));
+const faqBtns = Array.from(document.querySelectorAll(".faq-btn"));
 
-openMenuBtn.addEventListener('click', () => {
-     navMenu.classList.remove('translate-x-full')
-     overlay.classList.remove('hidden')
-})
+const setUp = () => {
+     openMenuBtn.addEventListener("click", () => {
+          navMenu.classList.remove("translate-x-full");
+          overlay.classList.remove("hidden");
+     });
 
-closeMenuBtn.addEventListener('click', closeMenu)
+     closeMenuBtn.addEventListener("click", closeMenu);
 
-navLinks.forEach((navLink) => {
-     navLink.addEventListener('click', closeMenu)
-})
+     navLinks.forEach((navLink) => {
+          navLink.addEventListener("click", closeMenu);
+     });
 
-function closeMenu() {
-     navMenu.classList.add('translate-x-full')
-     overlay.classList.add('hidden')
-}
+     function closeMenu() {
+          navMenu.classList.add("translate-x-full");
+          overlay.classList.add("hidden");
+     }
 
+     faqBtns.forEach((btn) => {
+          btn.addEventListener("click", () => {
+               btn.classList.toggle("bg-blue-600");
+               btn.classList.toggle("text-white");
+               const icon = btn.querySelector("i");
+               icon.className =
+                    icon.className === "bi bi-plus"
+                         ? "bi bi-dash"
+                         : "bi bi-plus";
+               const answer = btn.parentElement.nextElementSibling;
+               answer.classList.toggle("overflow-hidden");
+               answer.classList.toggle("h-0");
+          });
+     });
+};
+
+window.addEventListener("load", setUp);
